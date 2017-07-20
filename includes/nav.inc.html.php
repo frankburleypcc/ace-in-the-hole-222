512c50538cc5d5695861a041ea8c30973c9eb00e
frankburleypcc <FrankBurley@Franks-MacBook-Pro.local>
2017-07-20 10:22:06 -0700

    Code Refactoring
    Cleaned up the code and added comments. Small changes to test some
    styling. Still working with the grid system to hide some of the
    elements.

--- includes/nav.inc.html.php
+++ includes/nav.inc.html.php
@@ -1,25 +1,11 @@
-<!--<nav>
-    <ul>
-        <li><a href="/cas225/portlandhistoricaltours/index.php">Home</a></li>
-        <li><a href="/cas225/portlandhistoricaltours/about/index.php">About</a></li>
-        <li><a href="/cas225/portlandhistoricaltours/tours/index.php">Tours</a></li>
-        <li><a href="/cas225/portlandhistoricaltours/reservations/index.php">Reservations</a></li>
-        <li><a href="/cas225/portlandhistoricaltours/contact/index.php">Contact</a></li>
-    </ul>
-</nav>-->
+<!--this is the nav for the Ace in the Hole website. The includes code is included in each of the pages where the nav is to be displayed. There are two nav menus. One for mobile and one for tablet and desktop. The one for mobile will have the class .menu. The one for tablet and desktop will have the class .inlineNav-->
 <nav>
     <div class="menu">
         <ul>
-            <!--these are commented out because of the <li> locations. -->
-            <!--<a href="#"><li>Home</li></a>
-            <a href="#courses"><li>Courses</li></a>
-            <a href="#"><li>FAQ</li></a>
-            <a href="#"><li>Register</li></a>
-            <a href="#contact"><li>Contact</li></a>-->
             <li><a href="#">Home</a></li>
             <li><a href="#courses">Courses</a></li>
-            <li><a href="#">FAQ</a></li>
-            <li><a href="#">Register</a></li>
+            <li><a href="#faq">FAQ</a></li>
+            <li><a href="#registration">Register</a></li>
             <li><a href="#contact">Contact</a></li>
         </ul>
     </div> 
--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <!doctype html>
 <html lang="en">
 <head>
-    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Ace In The Hole</title>
     <!--google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
-    <!-- font awesome -->
+    <!-- font awesome for the social media buttons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/grid.css">
     <link rel="stylesheet" href="css/helper.css">
     <link rel="stylesheet" href="css/style.css"> 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    
-    <!--Fonts-->
-    
 </head>
 <body>
     <header>
@@ -43,31 +39,31 @@
     
     
 <main>
-     <img src="images/hero-placeholder.png">
+    <!--this is a placeholder image for the slideshow gallery-->
+    <img src="images/hero-placeholder.png">
     
     <section class="col span_9_of_12">
         <h2>About the Event</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim. Quisque fermentum laoreet mauris sed hendrerit. In nec dolor condimentum, condimentum tellus id, varius libero. In malesuada aliquet viverra. Vestibulum nulla metus, luctus a posuere id, convallis rhoncus lacus.
 
- </p>
+        </p>
     </section>
     
     <section class="col span_9_of_12">
         <h2>What to Bring?</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim. Quisque fermentum laoreet mauris sed hendrerit. In nec dolor condimentum, condimentum tellus id, varius libero. In malesuada aliquet viverra. Vestibulum nulla metus, luctus a posuere id, convallis rhoncus lacus.
-
- </p>
+        </p>
     </section>
+    <!--This is the sidebar. It will contain the social media feeds.--> 
     <aside class="col span_3_of_12" id="sidebar">
-    <h3>Social Media</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim. Quisque fermentum laoreet mauris sed hendrerit. In nec dolor condimentum, condimentum tellus id, varius libero. In malesuada aliquet viverra. Vestibulum nulla metus, luctus a posuere id, convallis rhoncus lacus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim. Quisque fermentum laoreet mauris sed hendrerit. In nec dolor condimentum, condimentum tellus id, varius libero. In malesuada aliquet viverra. Vestibulum nulla metus, luctus a posuere id, convallis rhoncus lacus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim. </p>
+        <h3>Social Media</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim. Quisque fermentum laoreet mauris sed hendrerit. In nec dolor condimentum, condimentum tellus id, varius libero. In malesuada aliquet viverra. Vestibulum nulla metus, luctus a posuere id, convallis rhoncus lacus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim. Quisque fermentum laoreet mauris sed hendrerit. In nec dolor condimentum, condimentum tellus id, varius libero. In malesuada aliquet viverra. Vestibulum nulla metus, luctus a posuere id, convallis rhoncus lacus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim.</p>
     
-</aside>
+    </aside>
     <section class="col span_9_of_12" id="courses">
         <h2>Courses</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim. Quisque fermentum laoreet mauris sed hendrerit. In nec dolor condimentum, condimentum tellus id, varius libero. In malesuada aliquet viverra. Vestibulum nulla metus, luctus a posuere id, convallis rhoncus lacus.
-
- </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim. Quisque fermentum laoreet mauris sed hendrerit. In nec dolor condimentum, condimentum tellus id, varius libero. In malesuada aliquet viverra. Vestibulum nulla metus, luctus a posuere id, convallis rhoncus lacus.
+            </p>
         <h3>Start Times</h3>
           <ul>
               <li>Lorem ipsum dolor sit amet</li>
@@ -78,38 +74,32 @@
               <li>Lorem ipsum dolor sit amet</li>
               <li>Lorem ipsum dolor sit amet</li>
               <li>Lorem ipsum dolor sit amet</li>
-        
-        
         </ul>
         <h3>Course Details</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim. Quisque fermentum laoreet mauris sed hendrerit. In nec dolor condimentum, condimentum tellus id, varius libero. In malesuada aliquet viverra. Vestibulum nulla metus, luctus a posuere id, convallis rhoncus lacus.
-
- </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim. Donec nec elit ex. Nullam maximus efficitur dignissim. Quisque fermentum laoreet mauris sed hendrerit. In nec dolor condimentum, condimentum tellus id, varius libero. In malesuada aliquet viverra. Vestibulum nulla metus, luctus a posuere id, convallis rhoncus lacus.
+            </p>
     </section>
-    <section class="col span_9_of_12">
+    <section class="col span_9_of_12" id="faq">
         <h2>FAQ</h2>
-        <dl>
-          <dt>Lorem ipsum dolor sit amet</dt>
-          <dd>- Lorem ipsum dolor sit amet</dd>
-          <dt>Lorem ipsum dolor sit amet</dt>
-          <dd>- Lorem ipsum dolor sit amet</dd>
-          <dt>Lorem ipsum dolor sit amet</dt>
-          <dd>- Lorem ipsum dolor sit amet</dd>
-          <dt>Lorem ipsum dolor sit amet</dt>
-          <dd>- Lorem ipsum dolor sit amet</dd>
-        </dl>
+            <dl>
+              <dt>Lorem ipsum dolor sit amet</dt>
+              <dd>- Lorem ipsum dolor sit amet</dd>
+              <dt>Lorem ipsum dolor sit amet</dt>
+              <dd>- Lorem ipsum dolor sit amet</dd>
+              <dt>Lorem ipsum dolor sit amet</dt>
+              <dd>- Lorem ipsum dolor sit amet</dd>
+              <dt>Lorem ipsum dolor sit amet</dt>
+              <dd>- Lorem ipsum dolor sit amet</dd>
+            </dl>
     </section>
-    <section class="col span_9_of_12">
+    <section class="col span_9_of_12" id="registration">
         <h2>Registration Form</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim..
-
-</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus condimentum pulvinar dignissim..
+            </p>
     </section>
     <section class="col span_9_of_12" id="contact">
         <h2>Contact Form</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
-</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </section>
 
 
@@ -120,34 +110,32 @@
 </main>
     
 
-    <footer>
-        <p>Copyright</p>
+<footer>
+    <!--The copyright will use php to generate the current year-->
+    <p>Copyright</p>
         <ul>
             <li>sm</li>
             <li>sm</li>
         </ul>
-        
-       
-        
-    </footer>
+</footer>
     
-    
-    <script>
-        $( ".cross" ).hide();
-        $( ".menu" ).hide();
-        $( ".hamburger" ).click(function() {
-        $( ".menu" ).slideToggle( "slow", function() {
-        $( ".hamburger" ).hide();
-        $( ".cross" ).show();
-        });
-        });
+<!--This script is for the hamburger menu-->  
+<script>
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+    });
+    });
 
-        $( ".cross" ).click(function() {
-        $( ".menu" ).slideToggle( "slow", function() {
-        $( ".cross" ).hide();
-        $( ".hamburger" ).show();
-        });
-        });
-    </script>
+    $( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).show();
+    });
+    });
+</script>
 </body>
 </html>
