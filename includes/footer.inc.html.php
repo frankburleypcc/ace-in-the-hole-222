44fe6129dc7cab4e33d5e91cc625517fc512326c
frankburleypcc <FrankBurley@Franks-MacBook-Pro.local>
2017-08-03 00:33:09 -0700

    Added content, social media feeds, php date
    Added content from the client assets. Social media feeds added to the
    sidebar with fixed heights. Added php to the footer to show the
    copyright year based on the current year.

--- includes/footer.inc.html.php
+++ includes/footer.inc.html.php
@@ -1,6 +1,6 @@
 <footer>
     <!--The copyright will use php to generate the current year-->
-    <p>Copyright</p>
+    <p>&copy; <?php echo date("Y"); ?></p>
         <ul>
             <li>sm</li>
             <li>sm</li>
